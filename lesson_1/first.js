9ea5e198ce9b887bd44aad6b7062317926393e97
Kosyura Olga <olyakosyura@gmail.com>
2015-11-24 14:07:52 +0300

    New 1

--- lesson_1/first.js
+++ lesson_1/first.js
@@ -51,4 +51,91 @@ document.addEventListener("DOMContentLoaded",function(){
 		
 	
 })
+
+window.document.onkeydown = function () {
+
+                        startRefocus();
+
+                }
+
+                function startRefocus(event) {
+
+                        event = event || window.event;
+
+                        //if (!event.ctrlKey) return;
+
+                        var key = event.keyCode;
+
+                        var targetElement = event.target || event.srcElement;
+
+                        focusMe(targetElement, key);
+
+                }
+
+                function focusMe(input, key) {
+
+                        var needFocusElement = true;
+
+                        function detectColumn(td) {
+
+                                var result = 0, x;
+
+                                while (td = td.previousElementSibling) {
+
+                                        ++result
+
+                                }
+
+                                return result;
+
+                        }
+
+                        try {
+
+                                switch (key) {
+
+                                        case 37:
+
+                                                needFocusElement = input.parentNode.previousElementSibling.querySelector("input");
+
+                                                break;
+
+                                        case 39:
+
+                                                needFocusElement = input.parentNode.nextElementSibling.querySelector("input");
+
+                                                break;
+
+                                       case 38:
+
+                                                needFocusElement = input.parentNode.parentNode.previousElementSibling.querySelectorAll("td")[detectColumn(input.parentNode)].querySelector("input");
+
+                                                break;
+
+                                        case 40:
+
+                                                needFocusElement = input.parentNode.parentNode.nextElementSibling.querySelectorAll("td")[detectColumn(input.parentNode)].querySelector("input");
+
+                        break;
+
+                    default:
+
+                        needFocusElement = false;
+
+                                }
+
+                        } catch (e) {
+
+                                needFocusElement = false;
+
+                        }
+
+  
+
+                        if (!needFocusElement) return;
+
+                        needFocusElement.focus();
+
+                }
+
 	
